6458934b908bb5af1ace00e804866b1d40e986d9
Nihal Konda <nihal.konda@sjsu.edu>
2019-09-16 06:03:42 -0700

    make it better

--- app.js
+++ app.js
@@ -18,10 +18,12 @@ var ref = db.ref("/AccidentReports");
 
 var cron = require('node-cron');
  
-cron.schedule('*/5 * * * * *', () => {
+var task = cron.schedule('*/5 * * * * *', () => {
   console.log('running a task every 5 secs');
     reportRandomAccident();
 });
+
+task.stop();
 
 app.get('/', function (req, res) {
     res.sendFile('views/index.html', {root: __dirname });
