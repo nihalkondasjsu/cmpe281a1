3596e12a5a2ac33b9df71de00e334d9bdd62cb64
Nihal Konda <nihal.konda@sjsu.edu>
2019-09-15 20:43:36 -0700

    make it better

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get('/', function (req, res) {
     console.log(ip);
     var geo = geoip.lookup(ip);
     res.send('<h1>'+JSON.stringify(geo)+'</h1>');
+    res.send("<iframe"+
+                "width='600'"+
+                "height='450'"+
+                "frameborder='0' style='border:0'"+
+                "src='https://www.google.com/maps/embed/v1/place?key=AIzaSyD6X0yRsNqENwXXrl4fdpMqA08RND_E3f0"+
+                "&location="+geo.ll[0]+","++geo.ll[1]++"' allowfullscreen>"+
+                "</iframe>"
+            );
 });
 app.listen( process.env.PORT || 3000 , function () {
   console.log('Example app listening on port '+(process.env.PORT || 3000)+'!');
