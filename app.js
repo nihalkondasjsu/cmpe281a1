be6b060f8a6182261270783dcc0b490904d9c1d4
Nihal Konda <nihal.konda@sjsu.edu>
2019-09-16 03:52:00 -0700

    make it better

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
 var express = require('express');
 var app = express();
 
-var geoip = require('geoip-lite');
-
-const requestIp = require('request-ip');
-
-const citiesDB = require('./db/citiesDB.json');
+const rdata = require('./lib/randomData.js');
 
 app.get('/', function (req, res) {
     res.setHeader('content-type', 'text/html');
-    var ip = "207.97.227.239";
-    console.log(ip);
-    ip = requestIp.getClientIp(req);
-    console.log(ip);
-    var geo = geoip.lookup(ip);
-    console.log(geo);
-    //res.send('<h1>'+JSON.stringify(geo)+'</h1>');
-    res.send("<iframe "+
-                "width='1100' "+
-                "height='800' "+
-                "frameborder='0' style='border:0'"+
-                "src='https://www.google.com/maps/embed/v1/place?key=AIzaSyD6X0yRsNqENwXXrl4fdpMqA08RND_E3f0"+
-                "&q="+geo.ll[0]+","+geo.ll[1]+"&zoom=2&center=33.7127,10' allowfullscreen>"+
-                "</iframe>"
-            );
+    res.send('<h1>Hello World</h1>');
 });
-app.listen( process.env.PORT || 3000 , function () {
-  console.log('Example app listening on port '+(process.env.PORT || 3000)+'!');
-    console.log(citiesDB.length);
-    var ip = "207.97.227.239";
-    var geo = geoip.lookup(ip);
 
-    console.log(geo);
+app.get('/reportRandomAccident', function (req, res) {
     
 });
 
+app.listen( process.env.PORT || 3000 , function () {
+  console.log('Example app listening on port '+(process.env.PORT || 3000)+'!');
+    
+    var admin = require("firebase-admin");
+
+    var serviceAccount = require("./lib/my-project.json");
+
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://my-project-1495237203835.firebaseio.com"
+    });
+
+    var db = admin.database();
+    var ref = db.ref("/AccidentReports");
+    
+    var val = rdata.randomAccident() ;
+    
+    ref.push(
+        val,
+        err => console.log(err ? 'error while pushing' : 'successful push')
+    );
+    
+    ref.once("value", function(snapshot) {
+      console.log(snapshot.val());
+    });
+    
+});
